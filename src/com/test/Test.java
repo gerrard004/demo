ad1052aa7497a3f3c3cd319d438214ceb7986ac4
gerrard004 <307168122@qq.com>
2017-12-07 11:05:26 +0800

    增加一个文件

---
+++ src/com/test/Test.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class Test {
+    private static Test ourInstance = new Test();
+
+    public static Test getInstance() {
+        return ourInstance;
+    }
+
+    private Test() {
+    }
+}
